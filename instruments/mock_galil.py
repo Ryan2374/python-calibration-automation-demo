35132032f52613fc3312e42f8b3facc4cebf2f73
Ryan2374 <rmmcdowell23@gmail.com>
2026-01-31 12:29:26 -0700

    Introduce Galil-style motion controller mock

--- instruments/mock_galil.py
+++
@@ -1,7 +0,0 @@
-class MockGalil:
-    def initialize(self):
-        print("Galil controller initialized.")
-
-    def set_voltage(self, voltage):
-        print(f"Setting voltage to {voltage}V")
-        return voltage
